8fe65f3e248bf6a51007fdde0af5c66b8fd9392a
RicardoMatsumura <fuzilo@hotmail.com>
2025-08-31 17:21:11 -0300

    Commit Inicial

---
+++ src/main/java/grupo/estudos/ingressoservice/IngressoServiceApplication.java
@@ -0,0 +1,13 @@
+package grupo.estudos.ingressoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grupo/estudos/ingressoservice/IngressoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo.estudos.ingressoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
